c29c405371c623faffd4b8e8f02f4fc2047c643d
Nihan Pol <nihan1593@gmail.com>
2019-09-16 11:49:09 -0400

    Make OS scripts compatible with general setup

--- 11yr_OS_2A_noBE.py
+++ 11yr_OS_2A_noBE.py
@@ -1,13 +1,13 @@
 from __future__ import division
 
-import pandas as pd, numpy as np, glob
+import numpy as np, glob
 import matplotlib.pyplot as pl
 
-import numpy as np, pandas as pd
+import numpy as np
 import glob, os as OS, json, sys
 import matplotlib.pyplot as plt
 import scipy.linalg as sl
-import pandas as pd
+import argparse
 
 import acor
 
@@ -37,59 +37,72 @@ def get_noise_from_enterprise(noisefile):
     
     return params
 
-realiz = np.int(sys.argv[1])
-loc = np.int(sys.argv[2])
+#Initialize the parser to accept user inputs
+parser = argparse.ArgumentParser(description = "Initiate detection run with model 2A")
 
-A_gwb_1 = np.linspace(1e-16, 1e-15, 20)
-A_gwb_2 = np.linspace(1.1e-15, 5e-15, 10)
+#Required arguments:
+parser.add_argument("-realiz", required = True, help = "Seed corresponding to the realization")
+parser.add_argument("-amp_index", required = True, type = int, help = "Index of the amplitude injected into the data set you're working with")
+parser.add_argument("-outdir", required = True, help = "Base directory to store output chain and parameter files")
+parser.add_argument("-chainpath", required = True, help = "Base directory where output MCMC chains are stored")
 
-A_gwb = np.append(A_gwb_1, A_gwb_2)
+#Optional arguments:
+parser.add_argument("--timpath", help = "Path to base directory holding timfiles of all realizations and amplitude injections; Default: /gpfs/scratch/nspol/real_injected_timfiles/", default = "/gpfs/scratch/nspol/real_injected_timfiles/")
+parser.add_argument("--parpath", help = "Path to directory containing all parfiles; Default: /gpfs/home/nspol/stochastic_11yr_analysis/data/partim/", default = "/gpfs/home/nspol/stochastic_11yr_analysis/data/partim/")
+parser.add_argument("--noisepath", help = "Path to directory holding noisefiles; Default: /gpfs/home/nspol/stochastic_11yr_analysis/data/noisefiles/", default = "/gpfs/home/nspol/stochastic_11yr_analysis/data/noisefiles/")
+parser.add_argument("--ephemeris", dest = 'ephem', help = "Choose solar system ephemeris for loading in pulsars; Default: DE436", choices = ['DE430', 'DE435', 'DE436', 'BayesEphem'], default = 'DE436')
+parser.add_argument("--useBE", dest = 'useBE', action = 'store_true', default = False, help = "Flag to use BayesEphem in detection run; Default: False")
 
-datadir = '/gpfs/home/nspol/stochastic_11yr_analysis/data/'
-realiz_dir = '/gpfs/scratch/nspol/real_injected_timfiles/realization_' + str(realiz) + '/'
-injection_dir = 'injecting_' + str(A_gwb[loc]) + '_gwb/'
+parser.add_argument("--gamma", dest = 'gamma',  help = "Specify index of stochastic GWB powerlaw function; Default: 13./3.", type = float, default = 13./3.)
+parser.add_argument("--nsamples", dest = 'nsamples', help = 'Number of samples in output chain; Default: 5e6', type = int, default = 5000000)
+parser.add_argument("--amps_path", dest = 'amps_path', help = "Path to numpy file containing array of injected stochastic GWB amplitudes; Default: ./injected_amps.npy", default = './injected_amps.npy')
+parser.add_argument("--psrlist", dest = 'psrlist', default = '', help = "Provide a text file of pulsar names to use in the detection analysis")
 
-parfiles = sorted(glob.glob(datadir + 'partim/*.par'))
-timfiles = sorted(glob.glob(realiz_dir + injection_dir + '*.tim'))
-#noisefiles = sorted(glob.glob(datadir + 'noisefiles/*.json'))
-psrlist = np.loadtxt("psrlist.txt", dtype = 'string')
+parser.add_argument("--orf", dest = 'orf', default = 'hd', help = "Set ORF for OS run ('hd', 'dipole', 'monopole'); Default: hd")
 
-noisefiles_path = datadir + 'noisefiles/'
+#Load the arguments:
+args = parser.parse_args()
+
+A_gwb = np.load(args.amps_path)
+
+parfiles = sorted(glob.glob(args.parpath + '*.par'))
+#timfiles = sorted(glob.glob(realiz_dir + injection_dir + '*.tim'))
+timfiles = sorted(glob.glob(args.timpath + '/realization_' + args.realiz + '/injecting_' + str(A_gwb[args.amp_index]) + '_gwb/*.tim'))
+noisefiles = sorted(glob.glob(args.noisepath + '*.json'))
+
+if args.psrlist:
+    psrlist = np.loadtxt(args.psrlist, dtype = 'string')
 
 psrs = []
-params = {}
-
 for p, t in zip(parfiles, timfiles):
-    psr = Pulsar(p, t, ephem='DE436')
+    if args.ephem in ['DE430', 'DE435', 'DE436']:
+        psr = Pulsar(p, t, ephem=args.ephem) #Cannot read in pulsars with BayesEphem
+    else:
+        psr = Pulsar(p, t, ephem = 'DE436')
     #Check if this pulsar has baseline > 3 years
-    psr_name = psr.name
-    if psr_name in psrlist:
-        
+    if psr.name in psrlist:
         psrs.append(psr)
-        full_path = noisefiles_path + '*' + str(psr_name) + '*'
-        noisefile = glob.glob(full_path)
-        
-        params.update(get_noise_from_enterprise(noisefile[0]))
-        
     else:
         continue
 
-chain_dir = '/scratch/nspol/real_injected_results/2a_wo_BE/' + \
-'realization_' + str(realiz) + '/' + 'injection_' + str(loc) + '/'
+params = {}
+for nfile in noisefiles:
+    params.update(get_noise_from_enterprise(nfile))
+
+chain_dir = args.chainpath + '/realization_' + args.realiz + '/injection_' + str(args.amp_index) + '/'
 
 chain = np.loadtxt(chain_dir + '/chain_1.txt')
 burn = int(0.25*chain.shape[0])
 pars = np.loadtxt(chain_dir + '/pars.txt', dtype=np.unicode_)
 
-os = optimal_statistic.OptimalStatistic(psrs, gamma_common = 13./3.,  orf = 'hd', bayesephem = False, noisedict = params)
+os = optimal_statistic.OptimalStatistic(psrs, gamma_common = args.gamma,  orf = args.orf, bayesephem = args.useBE, noisedict = params, select = 'backend')
 #os.pta.set_default_params(params)
 
-opt, snr = os.compute_noise_marginalized_os(chain[burn:, :], N = int(1e4))
+opt, snr = os.compute_noise_marginalized_os(chain[burn:, :], N = int(args.nsamples))
 
-outdir = '/gpfs/scratch/nspol/os_output/real_data/model_2a_noBE/'
-dirname = "realization_" + str(realiz) + '/'
+outdir = args.outdir + '/realization_' + args.realiz + '/'
 
-op_fname = outdir + dirname + 'amp_' + str(loc) + '.npz'
+op_fname = outdir + 'amp_' + str(args.amp_index) + '.npz'
 
 if not OS.path.exists(outdir + dirname):
     OS.makedirs(outdir + dirname)
--- 11yr_OS_2A_noBE.sh
+++ 11yr_OS_2A_noBE.sh
@@ -6,9 +6,9 @@
 #PBS -q stmcwilliams_lp
 ##PBS -q comm_mmem_week
 
-#PBS -t 0-29
+#PBS -t 0-14
 
-#PBS -N OS_real_injection_2A_${PBS_ARRAYID}
+#PBS -N OS_ipta_2A_${PBS_ARRAYID}
 
 module load compilers/gcc/8.2.0 compilers/python/2.7.13 libraries/suitesparse/5.3_gcc82 compilers/cuda/7.5
 module load libraries/openblas/0.3.2_gcc82 mpi/openmpi/3.1.2_gcc82
@@ -19,9 +19,13 @@ export OPENBLAS_NUM_THREADS=1
 export GOTO_NUM_THREADS=1
 export OMP_NUM_THREADS=1 
 
-cd /users/nspol/stochastic_11yr_analysis/notebooks/injection_analysis/
+cd /users/nspol/ipta_cut_dr2_injection_analysis/injection_analysis/
+
+#Stuff to avoid IO bomb
+rsync -av /users/nspol/.local/tempo2/share/tempo2 /dev/shm/
+export TEMPO2=/dev/shm/tempo2
 
 ##The first input to the python script is realization number [0,9] while the second is the index
 ##of the amplitude array [0, 29].
 
-python 11yr_OS_2A_noBE.py 0 ${PBS_ARRAYID}
+python 11yr_OS_2A_noBE.py -realiz 0 -amp_index ${PBS_ARRAYID} -chainpath /scratch/nspol/ipta_cut_results/2a_wo_BE/ -outdir /scratch/nspol/my_ipta_cut_results/2a_wo_BE_OS/ --psrlist /scratch/nspol/IPTA/partim_cut_IPTA/psrlist_cut_IPTA.txt --timpath /scratch/nspol/IPTA/ --parpath /scratch/nspol/IPTA/partim_cut_IPTA/ --noisepath /scratch/nspol/IPTA/partim_cut_IPTA/ --amps_path ipta_amps.npy
