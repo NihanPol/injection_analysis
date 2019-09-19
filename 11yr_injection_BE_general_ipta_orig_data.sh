bdc534b357f6d402fd3f0ac676174db17be2fe70
Nihan Pol <nihan1593@gmail.com>
2019-09-19 09:54:43 -0400

    Add code with similar setup, but to run on non-injected data

---
+++ 11yr_injection_BE_general_ipta_orig_data.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+#PBS -l nodes=1:ppn=2,pvmem=15gb
+##PBS -m abe
+#PBS -M noemail@hpc.wvu.edu
+##PBS -q stmcwilliams_lp
+##PBS -q comm_mmem_week
+#PBS -q standby
+
+#PBS -N orig_data_det_noBE
+
+module load compilers/gcc/8.2.0 compilers/python/2.7.13 libraries/suitesparse/5.3_gcc82 compilers/cuda/7.5
+module load libraries/openblas/0.3.2_gcc82 mpi/openmpi/3.1.2_gcc82
+
+#module load libraries/suitesparse/5.3_gcc82 libraries/openblas/0.3.2_gcc82 mpi/openmpi/3.1.2_gcc82
+
+export OPENBLAS_NUM_THREADS=1
+export GOTO_NUM_THREADS=1
+export OMP_NUM_THREADS=1 
+
+cd /users/nspol/ipta_cut_dr2_injection_analysis/injection_analysis
+
+#Stuff to avoid IO bomb
+rsync -av /users/nspol/.local/tempo2/share/tempo2 /dev/shm/
+export TEMPO2=/dev/shm/tempo2
+
+##The first input to the python script is realization number [0,9] while the second is the index
+##of the amplitude array [0, 29].
+
+python 11yr_injection_BE_general_ipta_orig_data.py -outdir /scratch/nspol/ipta_cut_results/orig_data_ul_wo_prob_psr_noBE/ --psrlist /scratch/nspol/IPTA/partim_cut_IPTA/trunc_psrlist_cut_IPTA_ranked.txt --timpath /scratch/nspol/IPTA/partim_cut_IPTA/ --parpath /scratch/nspol/IPTA/partim_cut_IPTA/ --noisepath /scratch/nspol/IPTA/partim_cut_IPTA/ --dm_var --dm_gp -ul
