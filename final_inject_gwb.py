3a0314765baeb84eae3dfab65d9c6f1001dd9792
Nihan Pol <nihan1593@gmail.com>
2019-11-22 17:18:27 -0500

    add functionality to read in pkl file to inject_gwb.py

--- final_inject_gwb.py
+++ final_inject_gwb.py
@@ -6,6 +6,7 @@ from collections import OrderedDict
 import libstempo as T2
 import libstempo.toasim as LT
 import argparse
+import pickle
 
 #Initialize the parser to accept user inputs
 parser = argparse.ArgumentParser(description = "Initiate data sets with stochastic GWB injections with given numpy seed (realization) and amplitudes")
@@ -16,6 +17,7 @@ parser.add_argument("-amps_path", dest = 'amps_path', required = True, help = "P
 parser.add_argument("-outdir", required = True, help = "Base directory to store output chain and parameter files")
 
 #Optional arguments:
+parser.add_argument("--read_pickle", dest = 'read_pickle', action = 'store_true', default = True, help = "Flag to read dataset as pickle to save i/o time; Default: True")
 parser.add_argument("--timpath", help = "Path to base directory holding timfiles; Default: /users/nspol/stochastic_11yr_analysis/data/partim/", default = "/users/nspol/stochastic_11yr_analysis/data/partim/")
 parser.add_argument("--parpath", help = "Path to directory containing all parfiles; Default: /users/nspol/stochastic_11yr_analysis/data/partim/", default = "/users/nspol/stochastic_11yr_analysis/data/partim/")
 parser.add_argument("--ephemeris", dest = 'ephem', help = "Choose solar system ephemeris for loading in pulsars; Default: DE436", choices = ['DE430', 'DE435', 'DE436', 'BayesEphem'], default = 'DE436')
@@ -25,14 +27,17 @@ parser.add_argument("--save_pickle", dest = 'save_pickle', action = 'store_true'
 #Load the arguments:
 args = parser.parse_args()
 
-parfiles = sorted(glob.glob(args.parpath+'*.par'))
-timfiles = sorted(glob.glob(args.timpath+'*.tim'))
-
-print("Loading in {} and {} par and tim files".format(len(parfiles),len(timfiles)))
-
-#Define different stochastic GWB amplitudes to be injected:
-
 A_gwb = np.load(args.amps_path)
+
+if args.read_pickle:
+    with open(args.timpath + "*.pkl") as f:
+        t2psr = pickle.load(f) 
+
+else:
+    parfiles = sorted(glob.glob(args.parpath+'*.par'))
+    timfiles = sorted(glob.glob(args.timpath+'*.tim'))
+
+    print("Loading in {} and {} par and tim files".format(len(parfiles),len(timfiles)))
 
 #Specify parent directory to hold all the realizations of the different injected amplitudes:
 outdir = args.outdir
@@ -56,16 +61,17 @@ for ii in seeds:
         #Initialize list to hold pulsar objects
         t2psr = []
         
-        for ii in range(len(parfiles)):
-            #Read in pulsar parfile and tim file and append to t2psr list
-            t2psr.append( T2.tempopulsar(parfile = parfiles[ii], timfile = timfiles[ii],
+        if not args.read_pickle:
+            for ii in range(len(parfiles)):
+                #Read in pulsar parfile and tim file and append to t2psr list
+                t2psr.append( T2.tempopulsar(parfile = parfiles[ii], timfile = timfiles[ii],
                                      maxobs=30000, ephem='DE436') )
 
-            #A fail-safe to catch any NaNs or infinites.
-            if np.any(np.isfinite(t2psr[ii].residuals())==False)==True:
-                t2psr[ii] = T2.tempopulsar(parfile = parfiles[ii], timfile = timfiles[ii])
+                #A fail-safe to catch any NaNs or infinites.
+                if np.any(np.isfinite(t2psr[ii].residuals())==False)==True:
+                    t2psr[ii] = T2.tempopulsar(parfile = parfiles[ii], timfile = timfiles[ii])
 
-            print('\r{0} of {1}'.format(ii+1,len(parfiles)))
+                print('\r{0} of {1}'.format(ii+1,len(parfiles)))
 
         #Inject the GWB into the pulsars:
         LT.createGWB(t2psr, Amp = A_gwb[loc], gam = args.gamma, seed = int(ii))
